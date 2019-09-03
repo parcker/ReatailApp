295bc900e10d9e2832d91786b27053fa4e0de53a
unknown <parckeranthony@gmail.com>
2019-09-03 08:18:57 -0700

    made change to application model

--- ormconfig.js
+++ ormconfig.js
@@ -8,13 +8,20 @@ const entitiesDir = isProd ? 'dist' : 'src'
 const migrationsDir = isProd ? 'dist/migration/*.js' : 'src/migration/*.ts'
 
 module.exports = {
-  type: "postgres",
-  entities: [
-    `${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`
-  ],
-  url: process.env.DATABASE_URL,
+  type: "mysql",
+  host:"127.0.0.1",
+  port:"3306"||process.env.SERVERPORT,
+  username:"root",
+  password:"d0tN3t123",
+  database:"retailstore",
+  synchronize:true,
+  logging:true,
   migrations: [migrationsDir],
   cli: {
     migrationsDir: 'src/migration'
-  }
+  },
+  entities: [
+    `${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`
+  ],
+ 
 }
