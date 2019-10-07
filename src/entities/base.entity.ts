16baaeed94810b9e447c383cddeeb2a7a90fc41d
unknown <parckeranthony@gmail.com>
2019-10-07 12:54:48 -0700

    made change to baseentityclass added new attribute

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -2,10 +2,10 @@ import {UpdateDateColumn} from 'typeorm';
 
 export abstract class BaseEntityClass{
 
-    @UpdateDateColumn({name : 'dateCreated',type: 'timestamp'})
+    @UpdateDateColumn({name : 'dateCreated',type: 'timestamp', default: () => "CURRENT_TIMESTAMP" })
     public dateCreated?: Date;
 
-    @UpdateDateColumn({name : 'dateUpdated',type: 'timestamp'})
+    @UpdateDateColumn({name : 'dateUpdated',type: 'timestamp', default: () => "CURRENT_TIMESTAMP" })
     public dateUpdated?: Date;
 
     public isDisabled:boolean;
