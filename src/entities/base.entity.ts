a3b9df99649d88bc791f3a91da23f9c2276236a6
unknown <parckeranthony@gmail.com>
2019-10-07 13:03:09 -0700

    made change to test baseentity

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,11 +1,11 @@
-import {UpdateDateColumn} from 'typeorm';
+import {UpdateDateColumn, CreateDateColumn} from 'typeorm';
 
 export abstract class BaseEntityClass{
 
-    @UpdateDateColumn({name : 'dateCreated',type: 'timestamp', default: () => "CURRENT_TIMESTAMP" })
+    @CreateDateColumn({ precision: 4, default: () => "CURRENT_TIMESTAMP" })
     public dateCreated?: Date;
 
-    @UpdateDateColumn({name : 'dateUpdated',type: 'timestamp', default: () => "CURRENT_TIMESTAMP" })
+    @UpdateDateColumn({ precision: 4, default: () => "CURRENT_TIMESTAMP" })
     public dateUpdated?: Date;
 
     public isDisabled:boolean;
