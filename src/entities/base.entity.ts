4cdddd46dde01788c52bd4edd345ffc712c86da1
unknown <parckeranthony@gmail.com>
2019-10-07 14:44:06 -0700

    made change 11

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -2,7 +2,7 @@ import {UpdateDateColumn, CreateDateColumn} from 'typeorm';
 
 export abstract class BaseEntityClass{
 
-    @CreateDateColumn({ type: "timestamp",precision: 4 /* or any other */, default: () => "CURRENT_TIMESTAMP" })
+    @CreateDateColumn({ type: "timestamp",precision: null /* or any other */, default: () => "CURRENT_TIMESTAMP" })
     public dateCreated?: Date;
 
     @UpdateDateColumn({type: "timestamp"})
