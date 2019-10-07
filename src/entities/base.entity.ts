5939999262ab7401abcbbde7483da355a42eaf84
unknown <parckeranthony@gmail.com>
2019-10-07 14:38:02 -0700

    made chage

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -2,7 +2,7 @@ import {UpdateDateColumn, CreateDateColumn} from 'typeorm';
 
 export abstract class BaseEntityClass{
 
-    @CreateDateColumn({type: "timestamp"})
+    @CreateDateColumn({ type: "timestamp",precision: 4 /* or any other */, default: () => "CURRENT_TIMESTAMP" })
     public dateCreated?: Date;
 
     @UpdateDateColumn({type: "timestamp"})
