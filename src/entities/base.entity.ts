d089ba5d76b7a2e34f6142bb8421f4d13893be97
unknown <parckeranthony@gmail.com>
2019-10-07 13:35:21 -0700

    made change to migration file

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -2,10 +2,10 @@ import {UpdateDateColumn, CreateDateColumn} from 'typeorm';
 
 export abstract class BaseEntityClass{
 
-    @CreateDateColumn({ precision: 4, default: () => "CURRENT_TIMESTAMP" })
+    @CreateDateColumn({ precision: null, type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
     public dateCreated?: Date;
 
-    @UpdateDateColumn({ precision: 4, default: () => "CURRENT_TIMESTAMP" })
+    @UpdateDateColumn({  precision: null, type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
     public dateUpdated?: Date;
 
     public isDisabled:boolean;
