c29ed97b90170838cf3816b1378f2717f61f12ea
Anthony.orji <anthony.orji@globalaccelerex.com>
2019-11-15 07:09:17 +0100

    made change to feature module

--- src/settings/features/features.module.ts
+++ src/settings/features/features.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FeaturesController } from './features.controller';
+import { FeaturesService } from './features.service';
 
 @Module({
-  controllers: [FeaturesController]
+  controllers: [FeaturesController],
+   providers: [FeaturesService],
+  exports:[FeaturesService]
 })
 export class FeaturesModule {}
