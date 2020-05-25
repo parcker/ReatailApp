434aed4718cc9d825a2a0233837cdfdfedb1785f
unknown <parckeranthony@gmail.com>
2020-05-25 10:09:18 +0100

    applied fix on account service and module

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from '../users/users.module';
 import { CompanyModule } from '../company/company.module';
 import { EmailModule } from '../shared/email/email.module';
 import { PayloadvalidationModule } from '../shared/payloadvalidation/payloadvalidation.module';
+import { ApiResponseModule } from '../shared/response/apiResponse.module';
 
 @Module({
   controllers: [AccountController],
   providers: [AccountService],
-  imports: [UsersModule,CompanyModule,EmailModule,PayloadvalidationModule]
+  imports: [UsersModule,CompanyModule,EmailModule,PayloadvalidationModule,ApiResponseModule]
 })
 export class AccountModule {}
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -6,6 +6,7 @@ import { CompanyService } from '../company/company.service';
 import { CreateCompanyDto } from '../app-Dto/usermgr/company/company.dto';
 import {EmailService} from '../shared/email/emailService';
 import { PayloadvalidationService } from '../shared/payloadvalidation/payloadvalidation.service';
+import { ApiResponseService } from '../shared/response/apiResponse.service';
 
 @Injectable()
 export class AccountService {
@@ -13,27 +14,24 @@ export class AccountService {
     constructor(private readonly userService: UsersService, 
         private readonly comapnyService: CompanyService,
         private readonly emailservice:EmailService,
-        private readonly payloadService: PayloadvalidationService) {}
+        private readonly payloadService: PayloadvalidationService,
+        private readonly apiResponseService: ApiResponseService) {}
 
     public async create(signup: SigupDto): Promise<any> {
       
         try
         {
             let validationResult = await this.payloadService.validateSignUpAsync(signup);
-           
-
             if(validationResult.IsValid)
             {
                 let user = await this.userService.validateUserEmail(signup.contactPerson.email);
                 if (user) 
                 {
                     
-                    let result= new ResponseObj<any>();
-                    result.message=`Email address already exist !! account cannot be created` ;
-                    result.status=false;
-                    result.result="";
-                    result.code=409;
-                    return result;
+                    return this.apiResponseService.FailedBadRequestResponse(
+                        `Email address already exist !! account cannot be created`,
+                        HttpStatus.BAD_REQUEST,'');
+
                 }
                 let model=new CreateCompanyDto();
                 model.comapanyName=signup.company.comapanyName;
@@ -65,12 +63,10 @@ export class AccountService {
                         let emaildata={token:response.result.id, name: response.result.firstName,url:process.env.EMAIL_ACTIVATIONLINK};
                         this.emailservice.sendmail(userinfo.email,'Ecorvids-Account','index.handlebars',emaildata);
                         
-                        let result= new ResponseObj<any>();
-                        result.message=`sign up completed check your email for activation link` ;
-                        result.status=true;
-                        result.result="";
-                        result.code=200;
-                        return result
+                        return this.apiResponseService.SuccessResponse(
+                            `sign up completed check your email for activation link`,
+                            HttpStatus.OK,'');
+
                     }
                        
                 }
@@ -89,14 +85,21 @@ export class AccountService {
             HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    async activateAccount(token:string):Promise<ResponseObj<string>>{
+    async activateAccount(token:string):Promise<any>{
 
         try{
 
             let response = await this.userService.activateuserAccount(token);
             return response;
         }
-        catch(error){return error;}
+        catch(error)
+        {  
+            console.log('Error Message',error,Date.now())
+            Logger.error(error);
+            return new 
+            HttpException({message: 'Process error while executing operation:',code:500, status:false},
+            HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
 }
