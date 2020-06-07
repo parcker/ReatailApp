00fc69fc5d107e57db9ce2fd3d20e5ad1f39d6b5
parcker <parckeranthony@gmail.com>
2020-06-07 18:38:21 +0100

    Created a new change on product and product configuration

--- src/app-Dto/product.dto.ts
+++ src/app-Dto/product.dto.ts
@@ -1,83 +1,77 @@
 
 import { ApiModelProperty, ApiModelPropertyOptional } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+export class ProductConfigurationDto {
 
-export class CreatProductDto{
-
-    
-    @ApiModelProperty({ required: true})
-    name: string;
-    @ApiModelProperty({ required: true})
-    itemcode: string;
-    @ApiModelPropertyOptional()
-    description: string;
-    @ApiModelPropertyOptional()
-    packingtype: string;
     @ApiModelPropertyOptional()
     packs: number;
     @ApiModelPropertyOptional()
     expiredenabled: boolean;
-    @ApiModelProperty({ required: true})
-    categoryId:string;
     @ApiModelPropertyOptional()
-     subcategoryId:string;
-   
+    pack: number;
+    @ApiModelPropertyOptional()
+    leadtime: number;
+    @ApiModelPropertyOptional()
+    public canexpire: boolean;
+    @ApiModelPropertyOptional()
+    public canbesold: boolean;
+    @ApiModelPropertyOptional()
+    public canbepurchased: boolean;
+    @ApiModelPropertyOptional()
+    public anypromo: boolean;
+}
+export class CreatProductDto {
+
+
+    @ApiModelProperty({ required: true })
+    name: string;
+    @ApiModelProperty({ required: true })
+    itemcode: string;
+    @ApiModelPropertyOptional()
+    description: string;
+    @ApiModelProperty({ required: true })
+    categoryId: string;
+    @ApiModelPropertyOptional()
+    subcategoryId: string;
+    @Type(() => ProductConfigurationDto)
+    @ApiModelPropertyOptional({ type: ProductConfigurationDto })
+    productconfiguration: ProductConfigurationDto
+
 }
 
-export class UpdateProductDto{
+export class UpdateProductDto {
 
-    
-    @ApiModelProperty({ required: true})
-    
+
+    @ApiModelProperty({ required: true })
     public name: string;
-
-    
-    @ApiModelProperty({ required: true})
-  
+    @ApiModelProperty({ required: true })
     public itemcode: string;
-
-    
-    
     @ApiModelPropertyOptional()
     public description: string;
-
-    
     @ApiModelPropertyOptional()
-  
     public packingtype: string;
-    
     @ApiModelPropertyOptional()
-    
     public packs: number;
-
     @ApiModelPropertyOptional()
-    
     public expiredenabled: boolean;
-
     @ApiModelPropertyOptional()
-    
     public isdisabled: boolean;
-    
-  
-    @ApiModelProperty({ required: true})
-  
-    public categoryId:string;
-
-   
-  
+    @ApiModelProperty({ required: true })
+    public categoryId: string;
     @ApiModelPropertyOptional()
-  
-    public subcategoryId:string;
+    public subcategoryId: string;
+
+    @Type(() => ProductConfigurationDto)
+    @ApiModelPropertyOptional({ type: ProductConfigurationDto })
+    productconfiguration: ProductConfigurationDto
 
 }
-export class ProductStatusDto{
+export class ProductStatusDto {
 
-    
-    @ApiModelProperty({ required: true})
+
+    @ApiModelProperty({ required: true })
     public status: boolean;
-
-  
-    @ApiModelProperty({ required: true})
-  
-    public productId:string;
+    @ApiModelProperty({ required: true })
+    public productId: string;
 
 }
---
+++ src/entities/productconfiguration.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne, OneToMany, Index } from 'typeorm';
+import { BaseEntityClass } from './base.entity';
+import { Product } from './product.entity';
+@Entity()
+export class ProductConfiguration extends BaseEntityClass {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    pack: number;
+    @Column()
+    leadtime: number;
+    @Column()
+    public canexpire: boolean;
+    @Column()
+    public canbesold: boolean;
+    @Column()
+    public canbepurchased: boolean;
+    @Column()
+    public anypromo: boolean;
+
+    @Index()
+    @OneToOne(type => Product, product => product.productconfiguration)
+    @JoinColumn()
+    product: Product;
+}
