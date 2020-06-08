c9e50f4fa9fa325dda38efde65764f85b8d21965
parcker <parckeranthony@gmail.com>
2020-06-08 11:59:40 +0100

    Added new endpoint to business and company module

--- src/adminboard/businesslocation/businesslocation.service.ts
+++ src/adminboard/businesslocation/businesslocation.service.ts
@@ -1,18 +1,18 @@
 import { Injectable, HttpException, HttpStatus, Logger} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, getRepository, DeleteResult } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Business, BusinessLocation } from "../../entities/business.entity";
-import { ResponseObj } from '../../shared/generic.response';
-import { IbusinessLocationDto } from '../../app-Dto/usermgr/company/company.dto';
+import { ApiResponseService } from '../../shared/response/apiResponse.service';
 
 
 @Injectable()
 export class BusinesslocationService {
 
     constructor(@InjectRepository(BusinessLocation)private readonly buisnesLocationRepository: Repository<BusinessLocation>,
-    @InjectRepository(Business)private readonly buisnessRepository: Repository<Business>) 
+    @InjectRepository(Business)private readonly buisnessRepository: Repository<Business>,
+    private readonly apiResponseService: ApiResponseService) 
     {}
-    async create(buisnessname:string,address:string,businessId:string,userId:string): Promise<any>{
+    async create(buisnesslocationname:string,address:string,businessId:string,userId:string): Promise<any>{
        
        try
         { 
@@ -20,27 +20,23 @@ export class BusinesslocationService {
             const business=await this.buisnessRepository.findOne({where:{Id:businessId}});
             if (!business) {
 
-                let result = new ResponseObj<string>();
-                result.message = `invalid or business  Id , no business  data found`;
-                result.status = false;
-                result.result = '';
-                return result;
+                return this.apiResponseService.FailedBadRequestResponse(
+                    `invalid or business  Id , no business  data found`,
+                    HttpStatus.BAD_REQUEST,'');
+               
             }
             let model=new BusinessLocation();
-            model.name=buisnessname;
+            model.name=buisnesslocationname;
             model.address=address;
             model.createdby=userId;
-            model.IsActive=true;
             model.updatedby='';
             model.business =business;
             model.isDisabled=false;
             let response= await this.buisnesLocationRepository.save(model);
-          
-            let result= new ResponseObj<BusinessLocation>();
-            result.message=`Business location created` ;
-            result.status=true;
-            result.result=response;
-            return result;
+            return this.apiResponseService.SuccessResponse(
+                `${buisnesslocationname} Business location created`,
+                HttpStatus.OK, response);
+           
 
 
         }catch (error) {
@@ -60,12 +56,10 @@ export class BusinesslocationService {
          { 
             
              let response= await this.buisnesLocationRepository.find();
-            
-             let result= new ResponseObj<IbusinessLocationDto[]>();
-             result.message=`Business location created` ;
-             result.status=true;
-             result.result=response;
-             return result;
+             return this.apiResponseService.SuccessResponse(
+                `${response.length} Business location data`,
+                HttpStatus.OK, response);
+       
  
  
          }catch (error) {
@@ -78,22 +72,17 @@ export class BusinesslocationService {
                 },
                     HttpStatus.INTERNAL_SERVER_ERROR);
         }
-     }
-     async getbusinesslocationBusinessId(businessId:string): Promise<any>{
+    }
+    async getbusinesslocationBusinessId(businessId:string): Promise<any>{
        
         try
          { 
             
              let response= await this.buisnesLocationRepository.find({ where: { businessId: businessId},order: { name: 'ASC' }});
-             for(var i = 0; i < response.length; i++)
-             { 
-                console.log(response[i]); // output: Apple Orange Banana
-             }
-             let result= new ResponseObj<BusinessLocation[]>();
-             result.message=`Business location created` ;
-             result.status=true;
-             result.result=response;
-             return result;
+             return this.apiResponseService.SuccessResponse(
+                `${response.length} Business location data`,
+                HttpStatus.OK, response);
+           
  
  
          }catch (error) {
@@ -106,7 +95,31 @@ export class BusinesslocationService {
                 },
                     HttpStatus.INTERNAL_SERVER_ERROR);
         }
-     }
+    }
+    async businesslocationStatus(businesslocationId:string,status:boolean,updatedby:string):Promise<any>{
+        try{
+            let response= await this.buisnesLocationRepository.findOne({where: { id: businesslocationId } });
+            if(response){
+                response.isDisabled=status;
+                response.updatedby=updatedby;
+                 await this.buisnesLocationRepository.save(response);
+                 return this.apiResponseService.SuccessResponse(
+                    `Business location is ${status ? 'Enabled' : 'Disabled'}`,
+                    HttpStatus.OK, response);
+            }
+            return this.apiResponseService.FailedBadRequestResponse(
+                `invalid or business location Id  Id , data found`,
+                HttpStatus.BAD_REQUEST,'');
+        }
+        catch (error) {
+            Logger.error(error);
+            return new HttpException({
+               message: 'Process error while executing operation:',
+               code: 500, status: false
+            },
+               HttpStatus.INTERNAL_SERVER_ERROR);
+         }
+    }
     
     
 }
